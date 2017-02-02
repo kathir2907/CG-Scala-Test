c975bb01535fadd8db54a250869fc4ee85c67cfe
kathir2907 <kathir2907@yahoo.com>
2017-02-02 14:27:12 +0000

    Update ShoppingCartCalculatorOrangeOfferTest.scala
    Mixed offer changes

--- src/test/scala/capgemini/hmrctest/ShoppingCartCalculatorOrangeOfferTest.scala
+++ src/test/scala/capgemini/hmrctest/ShoppingCartCalculatorOrangeOfferTest.scala
@@ -7,19 +7,19 @@ class ShoppingCartCalculatorOrangeOfferTest extends FlatSpec {
   val shoppingCartCalculator = new ShoppingCartCalculatorOrangeOffer()
 
   "ShoppingCartCalculatorOrangeOffer" should "checkout shopping cart for 1 Orange" in {
-    assert(shoppingCartCalculator.checkoutCart(List("Orange")) == 25)
+    assert(shoppingCartCalculator.checkoutCart(List("Orange")) == 0.25)
   }
 
   "ShoppingCartCalculatorOrangeOffer" should "checkout shopping cart for 2 Oranges" in {
-    assert(shoppingCartCalculator.checkoutCart(List("Orange","Orange")) == 50)
+    assert(shoppingCartCalculator.checkoutCart(List("Orange","Orange")) == 0.5)
   }
 
   "ShoppingCartCalculatorOrangeOffer" should "checkout shopping cart for 3 Oranges" in {
-    assert(shoppingCartCalculator.checkoutCart(List("Orange","Orange","Orange")) == 50)
+    assert(shoppingCartCalculator.checkoutCart(List("Orange","Orange","Orange")) == 0.5)
   }
 
   "ShoppingCartCalculatorOrangeOffer" should "checkout shopping cart for 4 Oranges" in {
-    assert(shoppingCartCalculator.checkoutCart(List("Orange","Orange","Orange","Orange")) == 75)
+    assert(shoppingCartCalculator.checkoutCart(List("Orange","Orange","Orange","Orange")) == 0.75)
   }
 
 }
