35ed032552a0539544daebba364dd7944b1fefb0
kathir2907 <kathir2907@yahoo.com>
2017-02-02 14:26:29 +0000

    Update ShoppingCartCalculatorAppleOfferTest.scala
    Mixed offer changes

--- src/test/scala/capgemini/hmrctest/ShoppingCartCalculatorAppleOfferTest.scala
+++ src/test/scala/capgemini/hmrctest/ShoppingCartCalculatorAppleOfferTest.scala
@@ -7,19 +7,20 @@ class ShoppingCartCalculatorAppleOfferTest extends FlatSpec {
   val shoppingCartCalculator = new ShoppingCartCalculatorAppleOffer()
 
   "ShoppingCartCalculatorAppleOffer" should "checkout shopping cart for 1 Apple" in {
-    assert(shoppingCartCalculator.checkoutCart(List("Apple")) == 60)
+    assert(shoppingCartCalculator.checkoutCart(List("Apple")) == 0.6)
   }
 
   "ShoppingCartCalculatorAppleOffer" should "checkout shopping cart for 2 Apples" in {
-    assert(shoppingCartCalculator.checkoutCart(List("Apple","Apple")) == 60)
+    assert(shoppingCartCalculator.checkoutCart(List("Apple","Apple")) == 0.6)
   }
 
   "ShoppingCartCalculatorAppleOffer" should "checkout shopping cart for 3 Apples" in {
-    assert(shoppingCartCalculator.checkoutCart(List("Apple","Apple","Apple")) == 120)
+    assert(shoppingCartCalculator.checkoutCart(List("Apple","Apple","Apple")) == 1.2)
   }
 
   "ShoppingCartCalculatorAppleOffer" should "checkout shopping cart for 4 Apples" in {
-    assert(shoppingCartCalculator.checkoutCart(List("Apple","Apple","Apple","Apple")) == 120)
+    assert(shoppingCartCalculator.checkoutCart(List("Apple","Apple","Apple","Apple")) == 1.2)
   }
 
 }
+
