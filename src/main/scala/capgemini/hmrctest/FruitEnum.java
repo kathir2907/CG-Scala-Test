335fb948c8c72fa560e5f5215d8905107abb1d36
kathir2907 <kathir2907@yahoo.com>
2017-02-02 14:23:05 +0000

    Update FruitEnum.java
    Mixed offer changes

--- src/main/scala/capgemini/hmrctest/FruitEnum.java
+++ src/main/scala/capgemini/hmrctest/FruitEnum.java
@@ -1,14 +1,18 @@
 package capgemini.hmrctest;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public enum FruitEnum {
 
-    APPLE("Apple",60),
-    ORANGE("Orange",25);
+    APPLE("Apple",new BigDecimal(0.60)),
+    ORANGE("Orange",new BigDecimal(0.25)),
+    BANANA("Banana",new BigDecimal(0.20));
 
     private String name;
-    private int cost;
+    private BigDecimal cost;
 
-    private FruitEnum(String name, int cost){
+    private FruitEnum(String name, BigDecimal cost){
         this.name = name;
         this.cost = cost;
     }
@@ -17,8 +21,8 @@ public enum FruitEnum {
         return name;
     }
 
-    public int getCost() {
-        return cost;
+    public BigDecimal getCost() {
+        return cost.setScale(2, RoundingMode.HALF_UP);
     }
 
 }
